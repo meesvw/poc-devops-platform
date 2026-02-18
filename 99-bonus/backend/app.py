cd804dbe644d3224b9c4ec05e82dd826e2a56ca3
mvw <73003273+meesvw@users.noreply.github.com>
2026-02-18 20:56:25 +0100

    Fix most issues with the bonus folder

---
+++ 99-bonus/backend/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+
+# Variables
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(
+    app.wsgi_app,
+    x_for=1,
+    x_proto=1,
+    x_host=1,
+)
+
+
+# Functions
+@app.route("/api")
+def api_health():
+    return {"status": "ok"}
+
+
+# Start and bind the Flask application to all IPs of the container
+app.run(host="0.0.0.0", port=5000)
